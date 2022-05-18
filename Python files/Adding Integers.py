27541949fbddb566707e0b0e537fa2e57ab094a8
jonadabbanks <71635581+jonadabbanks@users.noreply.github.com>
2022-05-18 08:52:23 +0100

    adding integers

---
+++ Python files/Adding Integers.py
@@ -0,0 +1,9 @@
+#Write a simple python program that adds 2 numbers together and output the result on your terminal.
+#Your python program should use variables to carry out the addition
+
+#creating and input for the two numbers
+no_1 =  int(input("Enter the first number: "))
+no_2 = int(input("enter the second number: "))
+#summing them up
+sum = int(no_1 + no_2)
+print("The sum is: ", sum)
